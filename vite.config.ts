3e531b55c198b1847c9a12c6a2860ba548b72148
karthi <kr748283@gmail.com>
2025-11-23 21:20:44 +0530

    major-fix-optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -10,6 +8,6 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [react(),tailwindcss()]
+      plugins: [react()]
     };
 });
